30f4dac046db88ff3d2bffd17c0fe299e964a0e6
Dener-arx <54905778+Dener-arx@users.noreply.github.com>
2022-12-10 15:09:24 -0300

    Update TemperatureClient.py

--- python/TemperatureClient.py
+++ python/TemperatureClient.py
@@ -13,19 +13,19 @@ def run():
 
         # Query Temperature by Date
         response = stub.GetTemperatureByDate(TemperatureService_pb2.TemperatureDate(date='25-02-2022'))
-        print ('Temperature\'s data: ' + str(response))
+        print ('Result of Query By Date:\n' + str(response) + '\n\n')
 
         # Query Temperature by Location
         response = stub.GetTemperatureByLocation(TemperatureService_pb2.TemperatureLocation(location='Sao Paulo'))
-        print ('Temperature\'s data: ' + str(response))
+        print ('Restult of Query By Location:\n' + str(response) + '\n\n')
 
         # Add New Temperature
         response = stub.CreateTemperature(TemperatureService_pb2.TemperatureData(date='12-03-2022', location='Rio de Janeiro', temperature=29.3))
-        print ('Added New Temperature ' + response.status)
+        print ('Added New Temperature ' + response.status + '\n\n')
 
         # List all Temperature
         response = stub.ListAllTemperatures(TemperatureService_pb2.EmptyMessage())
-        print ('All temperature: ' + str(response))
+        print ('List All temperature:\n' + str(response) + '\n\n')
 
 if __name__ == '__main__':
     logging.basicConfig()
