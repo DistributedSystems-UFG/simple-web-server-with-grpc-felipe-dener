ba5df3c7ef265775545b253fce43ac6577c32240
felipelagares <62161177+felipelagares@users.noreply.github.com>
2022-12-11 12:15:05 -0300

    Update TemperatureClient.py

--- python/TemperatureClient.py
+++ python/TemperatureClient.py
@@ -10,35 +10,39 @@ import const
 def run():
     with grpc.insecure_channel(const.IP+':'+const.PORT) as channel:
         print('services available: create, getByDate, getByLocation, listAll')
+        print('type break to exit or ctrl+c to interupt')
         stub = TemperatureService_pb2_grpc.TemperatureServiceStub(channel)
-        service = input('choose a service: ')
-        # Query Temperature by Date
-        if service.strip() == "getByDate":
-            print('date format: dd-mm-yyyy')
-            date = input('searching date: ')
-            response = stub.GetTemperatureByDate(TemperatureService_pb2.TemperatureDate(date=date))
-            print('Result of Query By Date:\n' + str(response) + '\n\n')
+        while True:
+            service = input('choose a service: ')
+            # Query Temperature by Date
+            if service.strip() == "getByDate":
+                print('date format: dd-mm-yyyy')
+                date = input('searching date: ')
+                response = stub.GetTemperatureByDate(TemperatureService_pb2.TemperatureDate(date=date))
+                print('Result of Query By Date:\n' + str(response) + '\n\n')
 
-        # Query Temperature by Location
-        if service.strip() == "getByLocation":
-            location = input('searching location: ')
-            response = stub.GetTemperatureByLocation(TemperatureService_pb2.TemperatureLocation(location=location))
-            print('Restult of Query By Location:\n' + str(response) + '\n\n')
-        
-        # Add New Temperature
-        if service.strip() == "create":
-            print('date format: dd-mm-yyyy')
-            date = input('date: ')
-            location = input('location: ')
-            temp = float(input('temperature: '))
-            response = stub.CreateTemperature(TemperatureService_pb2.TemperatureData(date=date, location=location, temperature=temp))
-            print('Added New Temperature ' + response.status + '\n\n')
+            # Query Temperature by Location
+            if service.strip() == "getByLocation":
+                location = input('searching location: ')
+                response = stub.GetTemperatureByLocation(TemperatureService_pb2.TemperatureLocation(location=location))
+                print('Restult of Query By Location:\n' + str(response) + '\n\n')
 
-        # List all Temperature
-        if service.strip() == 'listAll':
-            response = stub.ListAllTemperatures(TemperatureService_pb2.EmptyMessage())
-            print('List All temperature:\n' + str(response) + '\n\n')
-        
+            # Add New Temperature
+            if service.strip() == "create":
+                print('date format: dd-mm-yyyy')
+                date = input('date: ')
+                location = input('location: ')
+                temp = float(input('temperature: '))
+                response = stub.CreateTemperature(TemperatureService_pb2.TemperatureData(date=date, location=location, temperature=temp))
+                print('Added New Temperature ' + response.status + '\n\n')
+
+            # List all Temperature
+            if service.strip() == 'listAll':
+                response = stub.ListAllTemperatures(TemperatureService_pb2.EmptyMessage())
+                print('List All temperature:\n' + str(response) + '\n\n')
+
+            if service.strip() == 'break':
+                break
 
 if __name__ == '__main__':
     logging.basicConfig()
