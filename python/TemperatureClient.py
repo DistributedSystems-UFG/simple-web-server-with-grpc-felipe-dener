f8e9e8abaf89483e47add348473ad3f9224caf02
felipelagares <62161177+felipelagares@users.noreply.github.com>
2022-12-11 10:18:29 -0300

    Update TemperatureClient.py

--- python/TemperatureClient.py
+++ python/TemperatureClient.py
@@ -13,19 +13,19 @@ def run():
 
         # Query Temperature by Date
         response = stub.GetTemperatureByDate(TemperatureService_pb2.TemperatureDate(date='25-02-2022'))
-        print ('Result of Query By Date:\n' + str(response) + '\n\n')
+        print('Result of Query By Date:\n' + str(response) + '\n\n')
 
         # Query Temperature by Location
         response = stub.GetTemperatureByLocation(TemperatureService_pb2.TemperatureLocation(location='Sao Paulo'))
-        print ('Restult of Query By Location:\n' + str(response) + '\n\n')
+        print('Restult of Query By Location:\n' + str(response) + '\n\n')
         
         # Add New Temperature
         response = stub.CreateTemperature(TemperatureService_pb2.TemperatureData(date='12-03-2022', location='Rio de Janeiro', temperature=29.3))
-        print ('Added New Temperature ' + response.status + '\n\n')
+        print('Added New Temperature ' + response.status + '\n\n')
 
         # List all Temperature
         response = stub.ListAllTemperatures(TemperatureService_pb2.EmptyMessage())
-        print ('List All temperature:\n' + str(response) + '\n\n')
+        print('List All temperature:\n' + str(response) + '\n\n')
         
 
 if __name__ == '__main__':
